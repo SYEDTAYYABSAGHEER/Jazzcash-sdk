ecb6e34aa6e961e8aaea689779337b51dfd458c7
SYED TAYYAB SAGHEER <syedtayyabsagheer@gmail.com>
2025-01-02 14:09:21 +0500

    setup the gem project

---
+++ lib/jazz_cash.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jazz_cash/version"
+
+module JazzCash
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jazz_cash_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JazzCash do
+  it "has a version number" do
+    expect(JazzCash::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
